67cb6a63c18fa65a38ec71409d4bfc58dadc00b2
Devin Catuns <devincatuns1@gmail.com>
2024-08-13 15:45:27 -0400

    chore: moved BaseModal to root layout

--- frontend/src/app/(auth)/signin/page.js
+++ frontend/src/app/(auth)/signin/page.js
@@ -1,13 +1,10 @@
 import React from "react";
 import {LoginForm} from "@/features/users";
-import {auth} from "@/features/lib/auth";
 
 export default async function SignIn() {
-    const session = await auth();
     return (
         <>
             <LoginForm />
-            <p>Session: {JSON.stringify(session?.user)}</p>
         </>
     );
 }
