88a43a3b0ca4444457aac74d5c275c8fb399adca
Devin Catuns <devincatuns1@gmail.com>
2024-08-16 13:59:08 -0400

    fix: MaxUploadSizeExceededException: Maximum upload size exceeded

--- backend/src/main/java/xyz/streetscout/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/xyz/streetscout/exception/GlobalExceptionHandler.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import java.time.LocalDateTime;
 import java.util.stream.Collectors;
@@ -154,5 +155,20 @@ public class GlobalExceptionHandler {
         );
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<ErrorMessage> maxUploadSizeExceededExceptionHandler(
+            MaxUploadSizeExceededException e,
+            HttpServletRequest request
+    ) {
+        ErrorMessage errorMessage = new ErrorMessage(
+                request.getRequestURI(),
+                e.getMessage(),
+                HttpStatus.PAYLOAD_TOO_LARGE,
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body(errorMessage);
     }
 }
